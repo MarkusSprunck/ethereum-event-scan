230b270acc9412532caebb5af2f156b70c462786
MarkusSprunck <markus.sprunck@mnet-online.de>
2019-09-22 21:40:50 +0200

    Add license to source files

--- src/main.js
+++ src/main.js
@@ -1,13 +1,39 @@
 /**
+ * MIT License
+ *
+ * Copyright (c) 2019 Markus Sprunck (sprunck.markus@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+/**
  * Load all libraries
  */
 let Web3 = require('web3');             // Connect to Ethereum network
 let ZLib = require('zlib');             // Compression of abi file for URL parameter encoding
 let blockies = require('blockies');     // Render coloured images
 let $ = require("jquery");              // UI helpers
-global.jQuery = $;                      // Needed to run Bootstrap with jQuery
+global.jQuery = $;                          // Needed to run Bootstrap with jQuery
 require("bootstrap");                   // UI framework
 require("twbs-pagination");             // Paginator support
+
 
 /**
  * Global constants and message texts
