9f05a4a026b80cdd0748ffba5c0543e91b1946d4
MarkusSprunck <markus.sprunck@mnet-online.de>
2023-07-16 11:51:50 +0200

    Remove multi-platform build

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 npm run build
-docker buildx create --name mybuilder
-docker buildx use mybuilder
-docker buildx build --platform linux/amd64,linux/arm64 -t sprunck/ethereum-event-scan:latest --push .
+docker build -t sprunck/ethereum-event-scan:latest .
+docker push sprunck/ethereum-event-scan:latest
