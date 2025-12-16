5454bb30f2c749885e2894b1ed341975866bda33
Markus Sprunck <sprunck@muenchen.ihk.de>
2025-12-16 23:14:26 +0100

    Set version to 3.0.1

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 npm run build
-docker buildx create --name mybuilder
-docker buildx use mybuilder
-docker buildx build --platform linux/amd64,linux/arm64 -t sprunck/ethereum-event-scan:3.0.0 --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t sprunck/ethereum-event-scan:3.0.1 --push .
